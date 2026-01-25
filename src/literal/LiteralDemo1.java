faee98f1f9223e78d0fa24b9e10732d00ae237df
zyz-txy <1876195621@qq.com>
2026-01-26 01:33:57 +0800

    字符串

---
+++ src/literal/LiteralDemo1.java
@@ -0,0 +1,13 @@
+package literal;
+
+public class LiteralDemo1 {
+    //输出恐龙的信息：霸王龙 8岁 11.5 公
+    public static void main(String[] args) {
+        System.out.println("恐龙信息：");
+        System.out.println("名称：霸王龙");
+        System.out.println("年龄：8岁");
+        System.out.println("体重：11.5 ");
+        System.out.println("性别：公");
+    }
+
+}
