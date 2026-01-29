6dbc7919c165ccebdfed3194017c01e774f428fa
zyz-txy <1876195621@qq.com>
2026-01-29 16:30:17 +0800

    卡拉兹函数

--- src/ifdemo/ifDemo2.java
+++ src/ifdemo/ifDemo2.java
@@ -5,8 +5,12 @@ import java.util.Scanner;
 public class ifDemo2 {
     static void main() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("请输入一个数字：");
+        System.out.println("请输入一个正整数：");
+
         double number= sc.nextDouble();
+        if(number<0){
+            number = -number;
+        }
         if(number % 2 ==0){
             number = 3 * number + 1;
         }
