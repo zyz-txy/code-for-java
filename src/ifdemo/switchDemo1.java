5b7c3c029e1532c6deece7ad0da1501745256e6c
zyz-txy <1876195621@qq.com>
2026-01-29 17:30:59 +0800

    switch减肥计划+注意

--- src/ifdemo/switchDemo1.java
+++ src/ifdemo/switchDemo1.java
@@ -4,6 +4,14 @@ import java.util.Scanner;
 
 public class switchDemo1 {
     static void main() {
+        /*
+        switch的注意点
+        1.表达式：结果（字符/整数byte short int/枚举/字符串）
+        2.case:被匹配的值，只能是真实值不能是变量
+        3.case值不允许重复
+        4.default:没有匹配的case，执行default中的代码
+        5.break:结束switch
+         */
         Scanner sc = new Scanner(System.in);
         System.out.println("请输入星期几");
         int week= sc.nextInt();
