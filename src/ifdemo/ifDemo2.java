cc3a1c169221926ab20f646bde50eb35d26c3d81
zyz-txy <1876195621@qq.com>
2026-01-29 16:27:35 +0800

    卡拉兹函数

---
+++ src/ifdemo/ifDemo2.java
@@ -0,0 +1,18 @@
+package ifdemo;
+
+import java.util.Scanner;
+
+public class ifDemo2 {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个数字：");
+        double number= sc.nextDouble();
+        if(number % 2 ==0){
+            number = 3 * number + 1;
+        }
+        else{
+            number = number / 2;
+        }
+        System.out.println(number);
+    }
+}
