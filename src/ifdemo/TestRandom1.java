58077240216683ed0efb67e8dcb7c05f1ce94c0d
zyz-txy <1876195621@qq.com>
2026-01-30 23:24:01 +0800

    Random衍生2

--- src/ifdemo/TestRandom1.java
+++ src/ifdemo/TestRandom1.java
@@ -1,20 +1,40 @@
 package ifdemo;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class TestRandom1 {
     static void main() {
+        Scanner sc = new Scanner(System.in);
         //int n = r.nextInt();
         //小括号里面写一个数字n，则返回一个0到n-1的随机数
         //小括号里面写两个数符，则返回一个n到m的随机数，包含n不包含m
         Random r = new Random();
-        for (int i = 0; i < 100; i++) {
+        /*for (int i = 0; i < 100; i++) {
             int n = r.nextInt(1,10);//包含1不包含10
             System.out.println(n);
         }
         for (int i = 0; i < 100; i++) {
             int n = r.nextInt(10);//从0开始到9
             System.out.println(n);
-        }
+        }*/
+        int num = r.nextInt(101);
+        System.out.println("请输入数字");
+        int input;
+        boolean flag = false;
+        do {
+            input= sc.nextInt();
+            if(input==num){
+                System.out.println("恭喜你猜对了");
+                flag = true;
+            }else if (input > num){
+                System.out.println("你猜的数字太大了");
+                System.out.println("请重新输入");
+            }else {
+                System.out.println("你猜的数字太小了");
+                System.out.println("请重新输入");
+            }
+
+        }while(! flag);
     }
 }
