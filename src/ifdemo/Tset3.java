50127e02f0fb4f8c47cf34f0ef95e99cc9aba3e0
zyz-txy <1876195621@qq.com>
2026-02-02 23:05:37 +0800

    循环打印三角形

---
+++ src/ifdemo/Tset3.java
@@ -0,0 +1,19 @@
+package ifdemo;
+
+public class Tset3 {
+    static void main() {
+        //双循环打印三角形
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 5; i >=0; i--) {
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
