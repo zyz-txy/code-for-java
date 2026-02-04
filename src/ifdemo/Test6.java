af288b8796672e9dbe93a5949aea559b43199893
zyz-txy <1876195621@qq.com>
2026-02-04 22:04:05 +0800

    制表符

--- src/ifdemo/Test6.java
+++ src/ifdemo/Test6.java
@@ -1,4 +1,13 @@
 package ifdemo;
 
 public class Test6 {
+    static void main() {
+        //制表符
+        //长度可变大的大空格，打印表格可以上下对齐
+
+        System.out.println("name\tage\tgender");
+        System.out.println("张三\t18\t男");
+        System.out.println("李四\t19\t女");
+        System.out.println("王五\t20\t男");
+    }
 }
