c10cfa382234f247bee2cb720f8f5c48125b00fd
zyz-txy <1876195621@qq.com>
2026-02-07 22:34:47 +0800

    数组静态初始化

---
+++ src/arrdemo/arr1.java
@@ -0,0 +1,14 @@
+package arrdemo;
+
+import java.sql.SQLOutput;
+
+public class arr1 {
+    static void main() {
+        //定义三个数组分别储存三个人的姓名、年龄、身高
+        String[] name = {"zhangsan lisi wangwu"};//字符串不同
+        int[] age = {18,19,20};
+        double[] height = {1.75,1.80,1.65};
+
+    }
+
+}
